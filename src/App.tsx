e8d10c1216b2e42c9542e26bd890dfd9a63add9d
Mohammed Islam <mmislam101@gmail.com>
2021-04-02 18:37:12 -0700

    Fix react-native-gesture-handler for jest

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
-import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeContainer from './containers/home';
