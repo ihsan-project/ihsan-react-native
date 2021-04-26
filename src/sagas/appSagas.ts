b806b598b9170ddf2c6a4a75678a99e31ed53c1b
Mohammed Islam <mmislam101@gmail.com>
2021-04-25 21:30:33 -0700

    Make axios api call for settings

--- src/sagas/appSagas.ts
+++ src/sagas/appSagas.ts
@@ -2,6 +2,9 @@ import { takeEvery, put } from 'redux-saga/effects';
 import { APP_DID_LOAD, HYDRATE_APP_UNAUTH } from '../constants';
 import { hydrateAppUnauth as hydrateAction } from '../actions'
 
+import axios from 'react-native-axios';
+import config from 'react-native-config';
+
 export function* appDidLoad() {
   yield takeEvery(APP_DID_LOAD, beginUnauthHydration);
 }
@@ -15,6 +18,20 @@ export function* unAuthHydrating() {
   yield console.log("do other unauthenticated hydration here");
 }
 
-function * downloadSettings() {
+function* downloadSettings() {
   console.log("begin downloading settings...");
+
+  var instance = axios.create({
+    baseURL: config.API_BASE_URL,
+    timeout: 10000,
+    headers: { 'x-api-key': config.API_KEY }
+  });
+
+  // TODO: Abstract this into some helper class
+  instance.get('settings').then((res) => {
+    // TODO: Load this into redux
+    console.log("settings", res.data);
+  }).catch((err) => {
+    console.log("settings error:", err);
+  })
 }
